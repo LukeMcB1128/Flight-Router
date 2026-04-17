7f267f59524bd8f0024a4401ced2670c73b021b4
Luke Brittain <luke.brittain@gmail.com>
2026-04-17 12:23:32 -0500

    Temp change- Remove globe mode until switching between modes can be fixed

--- src/components/ViewModeSwitcher.jsx
+++ src/components/ViewModeSwitcher.jsx
@@ -4,7 +4,7 @@
  */
 
 const MODES = [
-  { key: 'globe',  label: 'Globe',  icon: '🌍' },
+  //{ key: 'globe',  label: 'Globe',  icon: '🌍' },
   { key: 'us-map', label: 'Map',    icon: '🗺️'  },
   { key: 'region', label: 'Region', icon: '🔍'  },
 ]
